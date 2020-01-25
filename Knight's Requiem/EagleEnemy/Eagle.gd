28460610fd189ae393869b9dc95d303883a7cc9a
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2020-01-25 13:23:16 -0600

    Fully implemented eagle enemy. Added instances on level 2

--- Knight's Requiem/EagleEnemy/Eagle.gd
+++ Knight's Requiem/EagleEnemy/Eagle.gd
@@ -5,7 +5,7 @@ var explosion_fx = preload("res://EnemyExplosion/EnemyExplosion.tscn")
 
 func _on_WeakspotArea_was_stomped():
 	var explosion_instance = explosion_fx.instance()
-	explosion_instance.position = position
+	explosion_instance.position = $EagleSprite.global_position
 	get_tree().root.add_child(explosion_instance)
 	queue_free()
 	pass
